0b5c6ad43bc230881616df77f19b3c3a27413fea
Michael Buzzard <m.buzzard87@gmail.com>
2021-11-04 18:27:36 -0400

    add player class

---
+++ DelegatesDemo/Player.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesDemo
+{
+
+    
+    class Player
+    {
+
+        public string PlayerName { get; set; }
+
+        public Player(string name)
+        {
+            PlayerName = name;
+        }
+
+        public void StartGame()
+        {
+            Console.WriteLine($"Spawning player with ID: {PlayerName}");
+        }
+
+        public void GameOver()
+        {
+            Console.WriteLine($"Removing player with ID: {PlayerName}");
+        }
+
+
+    }
+}
