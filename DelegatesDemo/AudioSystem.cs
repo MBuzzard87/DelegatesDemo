ee93aec90a9f1c0269689108064197460edb8fa3
Michael Buzzard <m.buzzard87@gmail.com>
2021-11-04 18:27:48 -0400

    add audiosystem class

---
+++ DelegatesDemo/AudioSystem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesDemo
+{
+    class AudioSystem
+    {
+
+        public void StartGame()
+        {
+            Console.WriteLine("Audio system started...");
+            Console.WriteLine("Playing audio...");
+        }
+
+        public void GameOver()
+        {
+            Console.WriteLine("Audio system stopped");
+        }
+
+
+    }
+}
