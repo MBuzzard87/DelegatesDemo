95d22d326837bbb200cd4edda56910e989aa71ab
Michael Buzzard <m.buzzard87@gmail.com>
2021-11-04 18:28:02 -0400

    add rendering engine class

---
+++ DelegatesDemo/RenderingEngine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesDemo
+{
+    class RenderingEngine
+    {
+
+        public void StartGame()
+        {
+            Console.WriteLine("Rendering engine started");
+            Console.WriteLine("Drawing Visuals....");
+        }
+
+        public void GameOver()
+        {
+            Console.WriteLine("Rendering engine stopped");
+        }
+
+    }
+}
