44e11cc3b431ef457f130a543be04590814e951c
Michael Buzzard <m.buzzard87@gmail.com>
2021-11-04 13:36:41 -0400

    filter/removeAll

--- DelegatesDemo/Program.cs
+++ DelegatesDemo/Program.cs
@@ -10,6 +10,29 @@ namespace DelegatesDemo
     {
         static void Main(string[] args)
         {
+            List<string> names = new List<string>() { "Mike", "Peters", "Munir", "Christian" };
+            
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+            
+            names.RemoveAll(Filter);
+
+            Console.WriteLine("\nAfter RemoveAll\n");
+
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+
+            Console.ReadKey();
+        }
+
+        static bool Filter(string s)
+        {
+            return s.Contains("i");
         }
     }
 }
